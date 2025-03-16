3c61724fc55e3bdb82cd695cc9ebd9d33081a111
CristiSlv1 <strugaricristian9@gmail.com>
2025-03-16 13:59:25 +0200

    Feat: test

--- Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
+++ Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff96d8b528c090f3698b86fe372416a0c1ae32d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffc04d961b7395a1bc8cf7c7d028b16fa105d6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
