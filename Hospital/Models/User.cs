bea2d9947560c86ce252f11968b1c406265af870
voda <vodaioan03@gmail.com>
2025-03-17 16:52:52 +0200

    feat:add User Model

---
+++ Hospital/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public string Role { get; set; }
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Cnp { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public User(int userId, string username, string password, string mail, string role, string name, DateOnly birthDate, string cnp, string address, string phoneNumber, DateTime registrationDate)
+        {
+            UserId = userId;
+            Username = username;
+            Password = password;
+            Mail = mail;
+            Role = role;
+            Name = name;
+            BirthDate = birthDate;
+            Cnp = cnp;
+            Address = address;
+            PhoneNumber = phoneNumber;
+            RegistrationDate = registrationDate;
+        }
+    }
+}
--- Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
+++ Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ffb06eba5304eb0272ec5ab625c5e003b7040fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0959ee586afedba2361fe7ee3b6ec253ef74ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
