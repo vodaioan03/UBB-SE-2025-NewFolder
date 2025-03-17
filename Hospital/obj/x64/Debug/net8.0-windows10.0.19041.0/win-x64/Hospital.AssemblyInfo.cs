2a98c62986739f6fff9e91ad755d67b2c90b9478
voda <vodaioan03@gmail.com>
2025-03-17 16:16:25 +0200

    commit

--- Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
+++ Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a535821a7cad7e27c7d6d20d7e0beee8dc09404d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd35411bd22a48207a2bee2c5a3629a845f9317")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
