a7aff25d3c758ed72579a37c4c1af575223ab240
voda <vodaioan03@gmail.com>
2025-03-17 16:33:59 +0200

    feat:add Admin Model

---
+++ Hospital/Models/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+        public int UserId { get; set; }
+
+        public Admin(int adminId, int userId)
+        {
+            AdminId = adminId;
+            UserId = userId;
+        }
+    }
+}
