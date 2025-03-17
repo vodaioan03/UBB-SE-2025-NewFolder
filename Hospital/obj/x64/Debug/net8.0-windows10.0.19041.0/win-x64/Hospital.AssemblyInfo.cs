94dd1f8b9a1e103db72c837a7555c55c93f6881e
voda <vodaioan03@gmail.com>
2025-03-17 16:31:38 +0200

    feat:add Shift model

--- Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
+++ Hospital/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/Hospital.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c0bc069d960ad9878f415fcda2a8e57980772b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7e9085c042541918c9483d9b2a32a0a826ad05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
