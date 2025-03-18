49302d88f6855593590e6919f7619bad855e3eae
Andreea410 <andreeaungur494@yahoo.com>
2025-03-18 16:01:19 +0200

    Feat: Implemented all the methods from the

--- Hospital/Managers/ShiftManagerModel.cs
+++ Hospital/Managers/ShiftManagerModel.cs
@@ -1,5 +1,7 @@
 ﻿using Hospital.DatabaseServices;
 using System;
+using Hospital.Exceptions;
+using Hospital.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +11,38 @@ namespace Hospital.Managers
 {
     class ShiftManagerModel
     {
-        private ShiftsDatabaseService ShiftsDatabaseService;
+        private readonly ShiftsDatabaseService _shiftsDatabaseService;
+        private List<Shift> _shifts;
 
         public ShiftManagerModel(ShiftsDatabaseService shiftsDatabaseService)
         {
-            ShiftsDatabaseService = shiftsDatabaseService;
+            _shiftsDatabaseService = shiftsDatabaseService;
+            _shifts = new List<Shift>();
         }
 
+        public void LoadShifts(int doctorID)
+        {
+            _shifts = _shiftsDatabaseService.GetShiftsByDoctorId(doctorID);
+        }
+
+        public List<Shift> GetShifts()
+        {
+            return _shifts;
+        }
+
+        public Shift GetShiftByDay(TimeSpan date)
+        {
+            Shift shift = _shifts.FirstOrDefault(s => s.StartTime == date);
+            if (shift == null)
+                throw new ShiftNotFoundException(string.Format("Shift not found for date {0}", date.ToString()));
+            return shift;
+        }
+
+        public Shift GetShiftByDoctorAndDate(int doctorId, DateTime date)
+        {
+            Shift shift = _shiftsDatabaseService.GetShiftByDoctorAndDate(doctorId, date);
+            return shift;
+        }
 
     }
 }
