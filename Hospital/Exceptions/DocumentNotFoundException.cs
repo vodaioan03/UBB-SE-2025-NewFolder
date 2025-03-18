add362fb5a6c2096b2d83d8b6a34f86f3cd44d06
tiutcristian <107301343+tiutcristian@users.noreply.github.com>
2025-03-18 21:35:20 +0200

    document handling done

---
+++ Hospital/Exceptions/DocumentNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Exceptions
+{
+    class DocumentNotFoundException : Exception
+    {
+        public DocumentNotFoundException(string message) : base(message) {}
+    }
+}
