dd623de37afac7c3528692e8ab11ce1c3b7db936
Andreea410 <andreeaungur494@yahoo.com>
2025-03-18 12:41:59 +0200

    Refactor: Moved the populate methods in the constructor

--- Hospital/DatabaseServices/ShiftsDatabaseService.cs
+++ Hospital/DatabaseServices/ShiftsDatabaseService.cs
@@ -12,26 +12,13 @@ namespace Hospital.DatabaseServices
     public class ShiftsDatabaseService
     {
         private Config configs;
+        private List<Shift> shifts; //Can be deleted later -> used for hard coding the data at the moment
+        private List<Schedule> schedules //Can be deleted later -> used for hard coding the data at the moment
 
         public ShiftsDatabaseService()
         {
             this.configs = Config.GetInstance();
-        }
-
-        private List<Schedule> populateSchedule()
-        {
-            List<Schedule> schedules = new List<Schedule>
-            {
-                new Schedule(1,1),
-                new Schedule(2,2),
-                new Schedule(3,4),
-                new Schedule(4,5)
-            };
-            return schedules;
-        }
-        private List<Shift> populateShifts()
-        {
-            List<Shift> shifts = new List<Shift>
+            this.shifts = new List<Shift> 
             {
                 new Shift(1, DateTime.Today.AddDays(10), new TimeSpan(8, 0, 0), new TimeSpan(20, 0, 0)),
                 new Shift(2, DateTime.Today.AddDays(12), new TimeSpan(20, 0, 0), new TimeSpan(8, 0, 0)),
@@ -40,23 +27,24 @@ namespace Hospital.DatabaseServices
                 new Shift(5, DateTime.Today.AddDays(18), new TimeSpan(8, 0, 0), new TimeSpan(20, 0, 0)),
                 new Shift(6, DateTime.Today.AddDays(19), new TimeSpan(8, 0, 0), new TimeSpan(8, 0, 0)),
                 new Shift(7, DateTime.Today.AddDays(21), new TimeSpan(20, 0, 0), new TimeSpan(8, 0, 0))
-            };
-            return shifts;
+            }; //Can be deleted later , after connecting to the database;
+
+            this.schedules = new List<Schedule>
+            {
+                new Schedule(1,1),
+                new Schedule(2,2),
+                new Schedule(3,4),
+                new Schedule(4,5)
+            }; //Can be deleted later , after connecting to the database;
         }
 
         public List<Shift> GetShiftsByDoctorId(int doctorId)
         {
-            List<Shift> shifts = this.populateShifts();
-            List<Schedule> schedules = this.populateSchedule();
-
             return schedules.Where(schedule => schedule.DoctorId == doctorId).Join(shifts, schedule => schedule.ShiftId, shift => shift.ShiftId, (schedule, shift) => shift).ToList();
         }
 
         public Shift GetShiftByDoctorAndDate(int doctorId, DateTime date)
         {
-            List<Shift> shifts = this.populateShifts();
-            List<Schedule> schedules = this.populateSchedule();
-
             Schedule schedule = schedules.FirstOrDefault(s => s.DoctorId == doctorId);
             if (schedule == null)
                 throw new ScheduleNotFoundException(string.Format("Schedule not found for doctor ID {0}", doctorId));
