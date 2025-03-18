3e57dcb029010ac52d22f9eae89ac8ffb12fbb67
Andreea410 <andreeaungur494@yahoo.com>
2025-03-18 17:37:37 +0200

    Fix: Fixed the EndTime in Shift and also changed from hardcoded inputs to database

--- Hospital/DatabaseServices/ShiftsDatabaseService.cs
+++ Hospital/DatabaseServices/ShiftsDatabaseService.cs
@@ -6,41 +6,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Hospital.DatabaseServices
 {
     public class ShiftsDatabaseService
     {
-        private readonly Config configs;
-        private List<Shift> shifts; //Can be deleted later -> used for hard coding the data at the moment
-        private List<Schedule> schedules; //Can be deleted later -> used for hard coding the data at the moment
-
+        private readonly Config _config;
+      
         public ShiftsDatabaseService()
         {
-            this.configs = Config.GetInstance();
-            this.shifts = new List<Shift> 
-            {
-                new Shift(1, DateTime.Today.AddDays(10), new TimeSpan(8, 0, 0), new TimeSpan(20, 0, 0)),
-                new Shift(2, DateTime.Today.AddDays(12), new TimeSpan(20, 0, 0), new TimeSpan(8, 0, 0)),
-                new Shift(3, DateTime.Today.AddDays(14), new TimeSpan(8, 0, 0), new TimeSpan(8, 0, 0)),
-                new Shift(4, DateTime.Today.AddDays(16), new TimeSpan(8, 0, 0), new TimeSpan(20, 0, 0)),
-                new Shift(5, DateTime.Today.AddDays(18), new TimeSpan(8, 0, 0), new TimeSpan(20, 0, 0)),
-                new Shift(6, DateTime.Today.AddDays(19), new TimeSpan(8, 0, 0), new TimeSpan(8, 0, 0)),
-                new Shift(7, DateTime.Today.AddDays(21), new TimeSpan(20, 0, 0), new TimeSpan(8, 0, 0))
-            }; //Can be deleted later , after connecting to the database;
-
-            this.schedules = new List<Schedule>
-            {
-                new Schedule(1,1),
-                new Schedule(2,2),
-                new Schedule(3,4),
-                new Schedule(4,5)
-            }; //Can be deleted later , after connecting to the database;
+            this._config = Config.GetInstance();
         }
 
         public List<Shift> GetShiftsByDoctorId(int doctorId)
         {
-            return schedules.Where(schedule => schedule.DoctorId == doctorId).Join(shifts, schedule => schedule.ShiftId, shift => shift.ShiftId, (schedule, shift) => shift).ToList();
+
+            const string query = @"
+            SELECT s.ShiftId, s.Date, s.StartTime, s.EndTime
+            FROM Shifts s
+            JOIN Schedules sch ON s.ShiftId = sch.ShiftId
+            WHERE sch.DoctorId = @DoctorId";
+
+            List<Shift> shifts = new List<Shift>();
+
+            using (SqlConnection conn = new SqlConnection(_config.DatabaseConnection))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@DoctorId", doctorId);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Shift shift = new Shift(reader.GetInt32(0),reader.GetDateTime(1),reader.GetTimeSpan(2),reader.GetTimeSpan(3));
+                            shifts.Add(shift);
+                        }
+                    }
+                }
+            }
+            return shifts;
         }
 
         public Shift GetShiftByDoctorAndDate(int doctorId, DateTime date)
