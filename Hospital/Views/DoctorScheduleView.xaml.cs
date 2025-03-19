96d1169b2509628cce89a643e49c20cb74cfc7c3
Andreea410 <andreeaungur494@yahoo.com>
2025-03-20 01:27:21 +0200

    Feat: Implemented the DoctorScheduleView.xaml and  DoctorScheduleView.xaml.cs

--- Hospital/Views/DoctorScheduleView.xaml.cs
+++ Hospital/Views/DoctorScheduleView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using System;
 using Hospital.Managers;
 using Hospital.ViewModels;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments;
 
 namespace Hospital.Views
 {
@@ -16,12 +18,31 @@ namespace Hospital.Views
 
         private DoctorScheduleViewModel _viewModel;
 
+        private CalendarView ScheduleCalendar;
+
         public DoctorScheduleView(AppointmentManagerModel appointmentManager, ShiftManagerModel shiftManager)
         {
-
             _appointmentManager = appointmentManager;
             _shiftManager = shiftManager;
             _viewModel = new DoctorScheduleViewModel(_appointmentManager, _shiftManager);
+
+            var rootGrid = new Grid();
+            this.Content = rootGrid;
+
+            ScheduleCalendar = new CalendarView
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                SelectionMode = CalendarViewSelectionMode.Multiple
+            };
+
+            rootGrid.Children.Add(ScheduleCalendar);
+            this.Content = rootGrid;
+
+
+
         }
+
+
     }
 }
