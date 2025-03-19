1c04fb6aa708e193e6f2e11a14c4811454e6a97b
Andreea410 <andreeaungur494@yahoo.com>
2025-03-20 01:01:19 +0200

    Feat: Added the DoctorScheduleView.xaml and DoctorScheduleView.xam.cs

---
+++ Hospital/Views/DoctorScheduleView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using Hospital.Managers;
+using Hospital.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Views
+{
+    class DoctorScheduleView : Window
+    {
+        private readonly AppointmentManagerModel _appointmentManager;
+        private readonly ShiftManagerModel _shiftManager;
+
+        private DoctorScheduleViewModel _viewModel;
+
+        public DoctorScheduleView(AppointmentManagerModel appointmentManager, ShiftManagerModel shiftManager)
+        {
+
+            _appointmentManager = appointmentManager;
+            _shiftManager = shiftManager;
+            _viewModel = new DoctorScheduleViewModel(_appointmentManager, _shiftManager);
+        }
+    }
+}
