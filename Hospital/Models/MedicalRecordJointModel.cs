2d7295f40303042620d14a4442f14fb24521162e
Cristian Tiut <tiutcristian@gmail.com>
2025-03-20 02:53:47 +0200

    #feat: adds department name to joint model
    - departmentId and departmentName added as fields in MedicalRecordJointModel

--- Hospital/Models/MedicalRecordJointModel.cs
+++ Hospital/Models/MedicalRecordJointModel.cs
@@ -13,18 +13,33 @@ namespace Hospital.Models
         public string PatientName { get; set; }
         public int DoctorId { get; set; }
         public string DoctorName { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
         public int ProcedureId { get; set; }
         public string ProcedureName { get; set; }
         public DateTime Date { get; set; }
         public string Conclusion { get; set; }
 
-        public MedicalRecordJointModel(int medicalRecordId, int patientId, string patientName, int doctorId, string doctorName, int procedureId, string procedureName, DateTime date, string conclusion)
+        public MedicalRecordJointModel(
+            int medicalRecordId, 
+            int patientId, 
+            string patientName, 
+            int doctorId, 
+            string doctorName,
+            int departmentId,
+            string departmentName,
+            int procedureId, 
+            string procedureName, 
+            DateTime date, 
+            string conclusion)
         {
             MedicalRecordId = medicalRecordId;
             PatientId = patientId;
             PatientName = patientName;
             DoctorId = doctorId;
             DoctorName = doctorName;
+            DepartmentId = departmentId;
+            DepartmentName = departmentName;
             ProcedureId = procedureId;
             ProcedureName = procedureName;
             Date = date;
