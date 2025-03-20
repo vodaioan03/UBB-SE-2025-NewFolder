44c704eb9f254d2f06f0e3f01546692dafe04f05
Cristian Tiut <tiutcristian@gmail.com>
2025-03-20 02:08:57 +0200

    makes medical records hisotry ui similar to design
    - mainwindow button text changed
    - stackpanels updated to respect the design
    - changed color of title bar

--- Hospital/Views/MedicalRecordsHistoryView.xaml.cs
+++ Hospital/Views/MedicalRecordsHistoryView.xaml.cs
@@ -1,4 +1,6 @@
 using Hospital.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 
 namespace Hospital.Views
@@ -13,10 +15,47 @@ namespace Hospital.Views
         public MedicalRecordsHistoryView(MedicalRecordsHistoryViewModel medicalRecordsHistoryViewModel)
         {
             this.InitializeComponent();
+            this.AppWindow.Resize(new(800, 600));
+            this.StyleTitleBar();
+
             _viewModel = medicalRecordsHistoryViewModel;
             this.MedicalRecordsPanel.DataContext = _viewModel;
+        }
 
-            this.AppWindow.Resize(new(800, 600));
+        private void StyleTitleBar()
+        {
+
+            // Get the title bar of the app window.
+            AppWindow m_Window = this.AppWindow;
+            AppWindowTitleBar m_TitleBar = m_Window.TitleBar;
+
+            // Set title bar colors.
+            m_TitleBar.ForegroundColor = Colors.White;
+            m_TitleBar.BackgroundColor = Colors.Green;
+
+            // Set button colors.
+            m_TitleBar.ButtonForegroundColor = Colors.White;
+            m_TitleBar.ButtonBackgroundColor = Colors.SeaGreen;
+
+            // Set button hover colors.
+            m_TitleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+            m_TitleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
+            m_TitleBar.ButtonPressedForegroundColor = Colors.Gray;
+            m_TitleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+
+            // Set inactive window colors.
+            // Note: No effect when app is running on Windows 10
+            // because color customization is not supported.
+            m_TitleBar.InactiveForegroundColor = Colors.Gainsboro;
+            m_TitleBar.InactiveBackgroundColor = Colors.SeaGreen;
+            m_TitleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+            m_TitleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
+
+
+            /*arrivalCalendarDatePicker.MinDate = DateTime.Today;
+            arrivalCalendarDatePicker.Date = DateTime.Today;
+            arrivalCalendarDatePicker.MaxDate = DateTime.Today.AddMonths(1);*/
+
         }
     }
 }
