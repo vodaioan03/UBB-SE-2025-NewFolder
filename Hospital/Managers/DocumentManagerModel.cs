a02374bad654ba28050a15c5fe7e3b6930e69571
tiutcristian <107301343+tiutcristian@users.noreply.github.com>
2025-03-22 13:22:46 +0200

    #feat: add zipping and downloading method
    + empty list if no medical record found instead of null

--- Hospital/Managers/DocumentManagerModel.cs
+++ Hospital/Managers/DocumentManagerModel.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO.Compression;
+using System.IO;
 using System.Threading.Tasks;
 using Document = Hospital.Models.Document;
 
@@ -22,7 +24,7 @@ namespace Hospital.Managers
             s_documentList = new List<Document>();
         }
 
-        public async Task<List<Document>> GetDocuments()
+        public List<Document> GetDocuments()
         {
             return s_documentList;
         }
@@ -43,17 +45,53 @@ namespace Hospital.Managers
             }
         }
 
-        public async Task LoadDocuments(int MedicalRecordId)
+        public void LoadDocuments(int MedicalRecordId)
         {
-            try
-            {
-                s_documentList = _documentDBService.GetDocumentsByMedicalRecordId(MedicalRecordId).Result;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading documents: {ex.Message}");
-            }
+            s_documentList = _documentDBService.GetDocumentsByMedicalRecordId(MedicalRecordId).Result;
         }
 
+        public async Task DownloadDocuments(int patientId)
+        {
+            List<string> filePaths = new List<string>();
+            foreach (Document document in s_documentList)
+            {
+                filePaths.Add(document.File);
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var filePath in filePaths)
+                    {
+                        if (System.IO.File.Exists(filePath))
+                        {
+                            var fileName = Path.GetFileName(filePath);
+                            var entry = archive.CreateEntry(fileName, CompressionLevel.Fastest);
+
+                            using (var entryStream = entry.Open())
+                            {
+                                using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                                {
+                                    await fileStream.CopyToAsync(entryStream);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            throw new DocumentNotFoundException($"Document not found at path: {filePath}");
+                        }
+                    }
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var zipFile = memoryStream.ToArray();
+                string zipFileName = $"Documents_{DateTime.Now.ToString("yyyyMMddHHmmss")}.zip";
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads", zipFileName);
+                File.WriteAllBytes(path, zipFile);
+
+                Process.Start("explorer.exe", "/select, " + path);
+            }
+        }
     }
 }
